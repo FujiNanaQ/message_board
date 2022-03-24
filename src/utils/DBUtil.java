b34ca4c67ce8adc1967ece5d7ed06b6e1b951db6
nana.sasaki <nana.j.g@outlook.jp>
2022-03-24 17:04:08 +0900

    getAllMessagesを追加

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -4,11 +4,11 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class DButil {
+public class DBUtil {
     private static final String PERSISTENCE_UNIT_NAME = "message_board";
     private static EntityManagerFactory emf;
 
-    private static EntityManager createEntityManager() {
+    public static EntityManager createEntityManager() {
         return getEntityManagerFactory().createEntityManager();
     }
 
